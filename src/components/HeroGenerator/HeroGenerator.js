c96a514ac4106edb83e2b6e55e254ec1f94179ae
Bolych <alexbolych64@gmail.com>
2023-02-16 19:18:57 +0600

    made herogenerator with expansions but checkbox doesn't work

--- src/components/HeroGenerator/HeroGenerator.js
+++ src/components/HeroGenerator/HeroGenerator.js
@@ -10,11 +10,74 @@ import {useEffect, useRef, useState} from "react";
 // };
 //
 
+// Checkbox
+
+let battleOfLegendsHeroes = ['Alice', 'Meduza', 'Sindbad', 'Arthur']
+let cobbleAndFogHeroes = ['Jackile', 'Scherlock', 'Drakula', 'Invisible']
+let robinHoodAndBigfootHeroes = ['Robin', 'Bigfoot']
+
 let heroes = [ 'Alice', 'Meduza', 'Sindbad', 'Arthur', 'Robin', 'Bigfoot', 'Red hood', 'Beowulf', 'Jackile', 'Scherlock', 'Drakula', 'Invisible']
 
+let heroesArr = []
+
+
+// trying checkbox in usestate
+
+const Checkbox = (props) => {
+
+    let [checked, setChecked] = useState(false);
+    heroesArr.flat()
+    const handleChange = (e) => {
+        if (e.target.checked === true) {
+            console.log(checked)
+            heroesArr.push(props.expansionPack)
+            console.log(heroesArr)
+        }
+
+           // if (e.target.checked === false)
+     if (e.target.checked === false)
+
+         {
+             heroesArr.splice( heroesArr.length -1)
+      //       let toRemove = [props.expansionPack]
+      // let newArr =     heroesArr.splice((el) => !toRemove.includes(el));
+
+             console.log(heroesArr)
+            }
+
+
+        setChecked(e => !e);
+    };
+
+    return (
+        <div>
+            <label>
+                <p>{props.name}</p>
+                <input
+                    type="checkbox"
+                    checked={checked}
+                    onChange={handleChange}
+                />
+                My Value
+            </label>
+
+            <p> {checked.toString()}</p>
+        </div>
+    );
+}
+
+
+
+
+
+
+// Generate Hero Function
 function randomHero () {
-    let random = Math.floor(Math.random() * heroes.length);
-    let result = heroes[random];
+
+
+console.log(heroesArr)
+    let random = Math.floor(Math.random() * heroesArr.length);
+    let result = heroesArr[random];
     return result
 };
 
@@ -23,19 +86,28 @@ function randomHero () {
 
 
 const HeroGenerator = () => {
+
+
+
     const [hero, setHero] = useState('who is your hero?')
-const generatedHeroe = () => {
+const generatedHero = () => {
+
         setHero(randomHero())
 }
-
-
     return <div>
+
+
         <div>
-            <button onClick={generatedHeroe}>generate</button>
+            <button onClick={generatedHero}>generate</button>
         </div>
         <div>{hero}</div>
-
-
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <div><Checkbox name='BoL' expansionPack="'Alice', 'Meduza', 'Sindbad', 'Arthur'"/></div>
+        <div><Checkbox name='Cobble And Fog' expansionPack="'Jackile', 'Scherlock', 'Drakula', 'Invisible'"/></div>
+        <div><Checkbox name='Robin Hood' expansionPack="'Robin', 'Bigfoot'"/></div>
     </div>
 }
 
