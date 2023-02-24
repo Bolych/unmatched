5fb461a731ed279f3f27cb10e459bc40059824cd
Bolych <alexbolych64@gmail.com>
2023-02-24 19:56:14 +0600

    23.02 before trying to connect firebase

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -3,7 +3,8 @@ const AboutPage = () => {
     return (
         <div>
          <h1>Unmatched fan site</h1>
-
+            <br/>
+<p>This site is just for fun, not for commercial purposes. And it is not affiliated with Restoration Games or Mondo Games. If you are owner of any image and you don't want this image to be here, just contact me via  alexbolych64@gmail.com</p>
         </div>
     )
 }
