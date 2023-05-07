52f6f64182b9b76574a1503eea1ea33c03c106e2
Bolych <alexbolych64@gmail.com>
2023-05-07 20:08:43 +0600

    07.05 updated main page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,5 +1,7 @@
 import ContactForm from "../FeedbackForm/FeedbackForm";
 import s from './AboutPage.module.css'
+import {NavLink} from "react-router-dom";
+import React from "react";
 
 const AboutPage = () => {
     return (
@@ -10,20 +12,21 @@ const AboutPage = () => {
             <br/>
             <p>&nbsp;This is Unmatched board game's fun site.</p>
             <br/>
-            <p>&nbsp;Playing with friend and don't know what hero to chose? You can try our hero random generator that depends on expansions you have. </p>
+            <p>&nbsp;Playing with friend and don't know what hero to chose? You can try our hero  <NavLink to='/calculator' className={s.navbutton}>Random generator</NavLink> that depends on expansions you have. </p>
+
             <br/>
-            <p>&nbsp;Wonder what hero is stronger? You can try our odds calculator. </p>
+            <p>&nbsp;Wonder what hero is stronger? You can try our <NavLink to='/calculator' className={s.navbutton}>Odds Calculator </NavLink></p>
             <br/>
-            <p>&nbsp;Also you can find information about heroes. Not only game stats, but their background and stats based on fans opinion.</p>
+            <p>&nbsp;Check out our heroes encyclopedia. You can find there not only game stats, but their background and stats, based on fans opinion.</p>
             <br/>
-<br/>
-<br/>
+            <br/>
+            <br/>
             <div className={s.headerEmpty}></div>
 
             <div className={s.disclaimer}>
-            <p>&nbsp;This site is just for fun, not for commercial purposes. And it is not affiliated with Restoration
-                Games or Mondo Games. If you have any suggestions or objections, found a mistake, or maybe you are owner of some art and
-                you don't want it to be here, just contact us.</p>
+                <p>&nbsp;This site is just for fun, not for commercial purposes. And it is not affiliated with Restoration
+                    Games or Mondo Games. If you have any suggestions or objections, found a mistake, or maybe you are owner of some art and
+                    you don't want it to be here, just contact us.</p>
             </div>
             <br/>
             <div className={s.feedback}>
